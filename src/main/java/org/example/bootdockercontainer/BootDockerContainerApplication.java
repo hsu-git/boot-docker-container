dee69263329befeff9fbef90aa174e0ad970ac45
hsu-git <hsu080206@gmail.com>
2025-05-21 09:22:25 +0900

    Initial commit

---
+++ src/main/java/org/example/bootdockercontainer/BootDockerContainerApplication.java
@@ -0,0 +1,13 @@
+package org.example.bootdockercontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDockerContainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootDockerContainerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bootdockercontainer/BootDockerContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bootdockercontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootDockerContainerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
